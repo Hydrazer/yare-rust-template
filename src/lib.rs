c1e0dcb7bdfe8b56a1e5145e82204ff05d72aa60
Hydrazer <explodingaerosal@gmail.com>
2021-09-22 16:09:01 -0600

    e

--- src/lib.rs
+++ src/lib.rs
@@ -7,35 +7,41 @@ use crate::wrappers::*;
 pub extern "C" fn tick(_tick: u32) {
   //   let spirits: Vec<Spirit> = get_spirits();
   //   let my_spirits: Vec<Spirit> = get_my_spirits();
-  let alive_spirits: Vec<Spirit> = get_alive_spirits();
-  let my_alive_spirits: Vec<Spirit> = get_my_alive_spirits();
+  // let alive_spirits: Vec<Spirit> = get_alive_spirits();
+  let my_spirit_vec: Vec<Spirit> = get_my_alive_spirits();
   //   let bases: Vec<Base> = get_bases();
   let stars: Vec<Star> = get_stars();
+
+  for i in &my_spirit_vec {
+    spirit.goto([100, 100]);
+  }
+
+
   //   let outposts: Vec<Outpost> = get_outposts();
   //   let player_id: usize = get_player_id();
 
-  let pos = &stars[0].pos;
-  for spirit in &my_alive_spirits {
-    spirit.goto(pos);
-    graphics::line(
-      spirit.pos,
-      *pos,
-      graphics::Colour {
-        red: 255,
-        green: 0,
-        blue: 255,
-        alpha: 1.0,
-      },
-    )
-  }
+  // let pos = &stars[0].pos;
+  // for spirit in &my_alive_spirits {
+  //   spirit.goto(pos);
+  //   graphics::line(
+  //     spirit.pos,
+  //     *pos,
+  //     graphics::Colour {
+  //       red: 255,
+  //       green: 0,
+  //       blue: 255,
+  //       alpha: 1.0,
+  //     },
+  //   )
+  // }
 
   // this is a mess, but it works lol (yes ima change it)
-  log(&*format!(
-    "=== STATS ===\n
-        FRIENDLY SPIRITS: {}\n
-        OTHER SPIRITS: {}\n
-        ",
-    my_alive_spirits.len(),
-    alive_spirits.len() - my_alive_spirits.len()
-  ));
+  // log(&*format!(
+  //   "=== STATS ===\n
+  //       FRIENDLY SPIRITS: {}\n
+  //       OTHER SPIRITS: {}\n
+  //       ",
+  //   my_alive_spirits.len(),
+  //   alive_spirits.len() - my_alive_spirits.len()
+  // ));
 }
